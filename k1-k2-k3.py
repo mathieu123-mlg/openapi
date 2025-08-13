78cd67136ee0707be4264b27799a1353094af582
Ryan Andriamahery <ryan@hei.school>
2025-08-13 08:57:03 +0300

    feat(k1-k2-k3): GET /home

--- k1-k2-k3.py
+++ k1-k2-k3.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/ping")
 def hello():
     return Response(content="pong", status_code=200, media_type="text/plain")
+
+
+@app.get("/home")
+def home():
+    return Response("<html><body><h1>Welcome home!</h1></body></html>", status_code=200, media_type="text/html")
