9f5f79bb6e34cf95496b57a4da774d3be372d839
Ryan Andriamahery <ryan@hei.school>
2025-08-13 09:12:30 +0300

    feat(k1-k2-k3): GET /posts and POST /posts

--- k1-k2-k3.py
+++ k1-k2-k3.py
@@ -1,7 +1,21 @@
+from datetime import datetime
+from typing import List
+
 from fastapi import FastAPI
-from starlette.responses import Response
+from pydantic import BaseModel
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: str
+
+
+post_stored: List[Post] = []
 
 
 @app.get("/ping")
@@ -12,6 +26,25 @@ def hello():
 @app.get("/home")
 def home():
     return Response("<html><body><h1>Welcome home!</h1></body></html>", status_code=200, media_type="text/html")
+
+
+@app.post("/posts")
+def create_post(post_payload: List[Post]):
+    post_stored.extend(post_payload)
+    post_serialized = serialize_posts()
+    return JSONResponse(content=post_serialized, status_code=201, media_type="application/json")
+
+
+def serialize_posts():
+    post_serialized = []
+    for p in post_stored:
+        post_serialized.append(p.model_dump())
+    return post_serialized
+
+
+@app.get("/posts")
+def list_posts():
+    return JSONResponse(content=serialize_posts(), status_code=200, media_type="application/json")
 
 
 @app.get("/{full_path:path}")
