619e2ed7bb85e6c29297059a678a235b0d26913a
Ryan Andriamahery <ryan@hei.school>
2025-08-13 08:08:42 +0300

    feat: GET /welcome with name request param

--- k4-k5.py
+++ k4-k5.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return Response(content="Hello world!", status_code=200, media_type="text/plain")
+
+@app.get("/welcome")
+def welcome(name: str):
+    return Response(content=f"Welcome {name}!" , status_code=200, media_type="text/plain")
